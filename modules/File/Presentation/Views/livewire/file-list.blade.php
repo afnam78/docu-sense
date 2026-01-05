35588a0b1aaad634e3d6ccc46c778105ead09f41
Afnan Amin <afnam78@gmail.com>
2026-01-05 13:07:07 +0100

    FileList style

--- modules/File/Presentation/Views/livewire/file-list.blade.php
+++ modules/File/Presentation/Views/livewire/file-list.blade.php
@@ -11,9 +11,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
                             <thead
-                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                class="text-xs text-gray-700 uppercase bg-gray-50">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
                                     Nombre
@@ -34,9 +34,9 @@
                             </thead>
                             <tbody>
                             @foreach($files as $file)
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                <tr class="bg-white border-b border-gray-200 hover:bg-gray-50 ">
                                     <th scope="row"
-                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                         {{ $file->name }}
                                     </th>
                                     <td class="px-6 py-4">
